e426cf88e7b9ce64830d99ccf806f34774cfc505
Robin <robingpeel@gmail.com>
2013-12-28 17:54:38 +0000

    Latest update:  New icon and xml screens

--- gen/com/scatterlogic/games/spyrunner/R.java
+++ gen/com/scatterlogic/games/spyrunner/R.java
@@ -31,6 +31,18 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int briefingscreen=0x7f030001;
+        public static final int mainmenu=0x7f030002;
+        public static final int missioncontrol=0x7f030003;
+        public static final int missionlog=0x7f030004;
+        public static final int missionselect=0x7f030005;
+        public static final int musicsettings=0x7f030006;
+        public static final int mysterybonus=0x7f030007;
+        public static final int reviewmissions=0x7f030008;
+        public static final int settings=0x7f030009;
+        public static final int splash=0x7f03000a;
+        public static final int spyprofile=0x7f03000b;
+        public static final int trackstats=0x7f03000c;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
