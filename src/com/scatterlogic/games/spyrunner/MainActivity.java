3b85301a639140bc6872c01c8b3a4e2036ba4329
Robin <robingpeel@gmail.com>
2013-12-29 09:50:55 +0000

    All buttons on Main Menu now navigating, picture added to Spy Profile

--- src/com/scatterlogic/games/spyrunner/MainActivity.java
+++ src/com/scatterlogic/games/spyrunner/MainActivity.java
@@ -6,19 +6,54 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 
+/* Main Menu class, composed on 29/12/2013
+ * Just a list of intents, followed by click listeners for some buttons
+ */
+
 public class MainActivity extends Activity {
 	Intent missionSelect;
+	Intent bonusFeatureSelect;
+	Intent spyProfileSelect;
+	Intent reviewMissionSelect;
+	Intent settingSelect;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		//here are the intents
 		missionSelect = new Intent(this, MissionSelectActivity.class);
+		bonusFeatureSelect = new Intent(this, BonusFeatureActivity.class);
+		spyProfileSelect = new Intent(this, SpyProfileActivity.class);
+		reviewMissionSelect = new Intent(this, ReviewMissionActivity.class);
+		settingSelect = new Intent(this, SettingsActivity.class);
 		
-		
+		//display the screen
 		setContentView(R.layout.activity_main);
 		
+		//and set up the listeners
 		findViewById(R.id.missionselectbutton).setOnClickListener(new OnClickListener() {
 		    public void onClick(View v) {
 		    	startActivity(missionSelect);
+		    } 
+		});
+		findViewById(R.id.bonusfeaturebutton).setOnClickListener(new OnClickListener() {
+		    public void onClick(View v) {
+		    	startActivity(bonusFeatureSelect);
+		    } 
+		});
+		findViewById(R.id.spyprofilebutton).setOnClickListener(new OnClickListener() {
+		    public void onClick(View v) {
+		    	startActivity(spyProfileSelect);
+		    } 
+		});
+		findViewById(R.id.reviewmissionsbutton).setOnClickListener(new OnClickListener() {
+		    public void onClick(View v) {
+		    	startActivity(reviewMissionSelect);
+		    } 
+		});
+		findViewById(R.id.settingsbutton).setOnClickListener(new OnClickListener() {
+		    public void onClick(View v) {
+		    	startActivity(settingSelect);
 		    } 
 		});
 	}
@@ -27,9 +62,4 @@ public class MainActivity extends Activity {
 	protected void onPostCreate(Bundle savedInstanceState) {
 		super.onPostCreate(savedInstanceState);
 	}
-	
-	protected void onPause(Bundle savedInstanceState){
-		super.onPause();
-	}
-
 }
