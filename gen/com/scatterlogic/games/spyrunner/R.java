e07fd62d7379dec7bc80ec9d456c8bf39a1d9b8f
Robin <robingpeel@gmail.com>
2013-12-29 00:09:02 +0000

    Main Screen Buttons Added

--- gen/com/scatterlogic/games/spyrunner/R.java
+++ gen/com/scatterlogic/games/spyrunner/R.java
@@ -23,11 +23,17 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int robin8bit=0x7f020001;
     }
     public static final class id {
-        public static final int dummy_button=0x7f070002;
         public static final int fullscreen_content=0x7f070000;
         public static final int fullscreen_content_controls=0x7f070001;
+        public static final int mission_select=0x7f070002;
+        public static final int mysterycontentbutton=0x7f070007;
+        public static final int reviewmissionsbutton=0x7f070006;
+        public static final int selectmissionbutton=0x7f070003;
+        public static final int settingsbutton=0x7f070005;
+        public static final int spyprofilebutton=0x7f070004;
     }
     public static final class layout {
         public static final int activity_bonus_feature=0x7f030000;
