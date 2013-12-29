d3fff6427fdba8242050c1b24875a985073f53ac
Robin <robingpeel@gmail.com>
2013-12-29 09:37:29 +0000

    Buttons in sensible place on main menu, top button now navigates

--- gen/com/scatterlogic/games/spyrunner/R.java
+++ gen/com/scatterlogic/games/spyrunner/R.java
@@ -26,14 +26,14 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int robin8bit=0x7f020001;
     }
     public static final class id {
+        public static final int bonusfeaturebutton=0x7f070004;
         public static final int fullscreen_content=0x7f070000;
         public static final int fullscreen_content_controls=0x7f070001;
         public static final int mission_select=0x7f070002;
-        public static final int mysterycontentbutton=0x7f070007;
+        public static final int missionselectbutton=0x7f070003;
         public static final int reviewmissionsbutton=0x7f070006;
-        public static final int selectmissionbutton=0x7f070003;
-        public static final int settingsbutton=0x7f070005;
-        public static final int spyprofilebutton=0x7f070004;
+        public static final int settingsbutton=0x7f070007;
+        public static final int spyprofilebutton=0x7f070005;
     }
     public static final class layout {
         public static final int activity_bonus_feature=0x7f030000;
