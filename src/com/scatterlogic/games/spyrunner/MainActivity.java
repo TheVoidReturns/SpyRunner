dad65d9f3bc95371f1857273cbeeb6c3b451a508
Robin <robingpeel@gmail.com>
2013-12-29 00:34:01 +0000

    Compilable version of the previous

--- src/com/scatterlogic/games/spyrunner/MainActivity.java
+++ src/com/scatterlogic/games/spyrunner/MainActivity.java
@@ -114,8 +114,8 @@ public class MainActivity extends Activity {
 		// Upon interacting with UI controls, delay any scheduled hide()
 		// operations to prevent the jarring behavior of controls going away
 		// while interacting with the UI.
-		findViewById(R.id.mission_select).setOnTouchListener(
-				mDelayHideTouchListener);
+		//findViewById(R.id.mission_select).setOnTouchListener(
+		//		mDelayHideTouchListener);
 	}
 
 	@Override
